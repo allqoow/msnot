ab7a039d8c532f6d95be5895edd4eaada9ffefe0
cheukusi <cheukusi.kim@gmail.com>
2016-07-25 21:29:29 +0900

    it's me

--- treeGen/tgCase0/__init__.py
+++ treeGen/tgCase0/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Author    : Your developer name
-# Contact   : Your eMail
-# Started on: 201607??(yyyymmdd)
+# Author    : cheukusi
+# Contact   : cheukusi.kim@gmail.com
+# Started on: 20160725
 # Project	: msnot
 
 import re
@@ -22,9 +22,7 @@ class tgCase0():
 
 		self.process()
 	def process(self):
-		print "저는 Case0을 맡고 있습니다!"
-		print "저는 진지합니다."
-		print "ㅖㅣㅖ"
+		print "저는 Case2을 맡고 있습니다!"
 		auxList2 = []
 		self.schemeIndex0 = self.cddCmpntList[0][0]
 		self.auxList2 = self.cddCmpntList
