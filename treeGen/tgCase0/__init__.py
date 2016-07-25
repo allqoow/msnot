c0b37cda5854b494894a12b46bbf5073303e32cb
cheukusi <cheukusi.kim@gmail.com>
2016-07-25 21:36:05 +0900

    s

--- treeGen/tgCase0/__init__.py
+++ treeGen/tgCase0/__init__.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 #
 <<<<<<< HEAD
+<<<<<<< HEAD
 # Author    : cheukusi
 # Contact   : cheukusi.kim@gmail.com
 # Started on: 20160725
+=======
+# Author    : moon-eunseo
+# Contact   : aristocat1703@gmail.com
+# Started on: 20160725(yyyymmdd)
+>>>>>>> bfe8310ff4b6ec5d9406c333c8bd979f595a8468
 =======
 # Author    : moon-eunseo
 # Contact   : aristocat1703@gmail.com
